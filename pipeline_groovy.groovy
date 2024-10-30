4035dd1f5b4ee802a088792d9f22c9a5fd7cf79f
usama1 <usama>
2024-10-30 15:47:18 +0500

    update

--- pipeline_groovy.groovy
+++ pipeline_groovy.groovy
@@ -1,8 +1,8 @@
 // Define a function to create a pipeline for each branch
-def createPipeline(branchName, fileName) {
+def createPipeline(branchName, scriptName) {
     job("pipeline-${branchName}") {
         description("Pipeline for ${branchName} branch.")
-        
+
         scm {
             git("https://github.com/Usama-Malik123/maven-simple.git") {
                 branch(branchName)
@@ -14,13 +14,12 @@ def createPipeline(branchName, fileName) {
         }
 
         steps {
-            shell("echo 'Running ${fileName} in ${branchName} branch'")
-            // You can add additional steps to execute the specific files here
+            groovyCommand(scriptName) // Execute the specific script for the branch
         }
     }
 }
 
-// Create pipelines for each branch
+// Create pipelines for each branch with their respective scripts
 createPipeline('master', 'demo')
 createPipeline('feature', 'test')
 createPipeline('feature1', 'test1')
